27e548e2360a10c9f24950ae631256a948d71e57
adKosskowski <adam.kossakowski@outlook.com>
2018-05-08 00:17:34 +0200

    Nowy layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  // new TradingView.widget();
+
 }
+
+// <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async>
+// {
+//   "symbols": [
+//     {
+//       "title": "BTC/USD",
+//       "proName": "BITFINEX:BTCUSD"
+//     },
+//     {
+//       "description": "BTC/EUR",
+//       "proName": "BITFINEX:BTCEUR"
+//     },
+//     {
+//       "description": "BTC/GBP",
+//       "proName": "BITFINEX:BTCGBP"
+//     }
+//   ],
+//   "locale": "pl"
+// }
+// </script>
